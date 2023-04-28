bcd00aa0ee3fd0c8d1020afc6f24afac843e452f
AYC93 <ayc.leo@gmail.com>
2023-04-28 09:40:57 +0800

    sql uploaded

--- assessment_template/database/create.sql
+++ assessment_template/database/create.sql
@@ -1,3 +1,13 @@
 -- Task 1
 -- Write SQL statements in this file to create the brewery database and 
 -- populate the database with the given SQL files
+
+mysql -hcontainers-us-west-26.railway.app -uroot -pgVDZMqxqVi49mCLM9lbP --port 7327 --protocol=TCP railway 
+
+exit
+
+mysql -hcontainers-us-west-26.railway.app -uroot -pgVDZMqxqVi49mCLM9lbP --port 7327 --protocol=TCP railway < beers.sql
+mysql -hcontainers-us-west-26.railway.app -uroot -pgVDZMqxqVi49mCLM9lbP --port 7327 --protocol=TCP railway < breweries.sql
+mysql -hcontainers-us-west-26.railway.app -uroot -pgVDZMqxqVi49mCLM9lbP --port 7327 --protocol=TCP railway < categories.sql
+mysql -hcontainers-us-west-26.railway.app -uroot -pgVDZMqxqVi49mCLM9lbP --port 7327 --protocol=TCP railway < geocodes.sql
+mysql -hcontainers-us-west-26.railway.app -uroot -pgVDZMqxqVi49mCLM9lbP --port 7327 --protocol=TCP railway < styles.sql
